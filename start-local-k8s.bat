736cd6e3e53c57575537324c9280600b4bedb5b6
Paulo José Rocha Dantas <pjr_dantas@hotmail.com>
2026-03-26 23:54:18 -0300

    teste sonar

--- start-local-k8s.bat
+++ start-local-k8s.bat
@@ -3,29 +3,23 @@ echo ==========================================
 echo INICIANDO AMBIENTE LOCAL KUBERNETES
 echo ==========================================
 
-:: 1. Compila o Java e gera o projeto-piloto-0.0.1-SNAPSHOT.jar
 echo Gerando novo JAR...
 call ./mvnw clean package -DskipTests
 
-:: 2. Cria a imagem Docker que o Kubernetes vai usar
 echo Gerando nova imagem Docker...
 docker build -t projeto-piloto:latest .
 
-:: 3. Garante que o contexto e o docker-desktop
 kubectl config use-context docker-desktop
 
-:: 4. Aplica as configuracoes (Nginx e App)
 kubectl apply -f k8s/nginx-config.yaml
 kubectl apply -f k8s/deployment.yaml
 
-:: 5. Comando essencial: Força o K8s a ler a imagem que acabamos de criar
-kubectl rollout restart deployment projeto-piloto-svc
+:: Reinicia o DEPLOYMENT (correto)
+kubectl rollout restart deployment/projeto-piloto
 
-:: Aguarda os pods subirem
 echo Aguardando inicializacao...
 timeout /t 15
 
-:: Mostra como estao os pods
 kubectl get pods
 
 echo ==========================================
