d145b4c5df6f4823ae9087be60fbb2aa951b1e55
PJRDANTAS <PJRDANTAS@users.noreply.github.com>
2026-05-01 20:27:25 -0300

    Revert "Merge pull request #14 from pjrdantas/codex/migrate-project-from-oracle-to-postgresql-v71fum"
    This reverts commit 2df10fd1f9e2bb3866a1d3751b623440e7eace56, reversing
    changes made to 13c528e6b50e34699611b807c6922dde023aece7.

--- src/main/java/br/com/projeto/piloto/aplicativos/adapter/in/web/dto/AuthPerfilResumoDTO.java
+++
@@ -1,6 +0,0 @@
-package br.com.projeto.piloto.adapter.in.web.dto;
-
-public record AuthPerfilResumoDTO(
-    Long id,
-    String nmPerfil
-) {}
--- src/main/java/br/com/projeto/piloto/aplicativos/adapter/in/web/dto/RefreshTokenRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.projeto.piloto.adapter.in.web.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenRequestDTO(
-    @NotBlank(message = "refreshToken é obrigatório") String refreshToken
-) {}
