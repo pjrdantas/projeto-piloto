c3a8e9cde9d623b45def6cf3bf3776b27d8b41de
Paulo Dantas <20131563+pjrdantas@users.noreply.github.com>
2026-05-04 23:51:31 -0300

    Restaura AuthUsuarioUseCasePort como alias para resolver conflitos de merge

---
+++ src/main/java/br/com/projeto/piloto/accesscontrol/application/port/in/AuthUsuarioUseCasePort.java
@@ -0,0 +1,9 @@
+package br.com.projeto.piloto.accesscontrol.application.port.in;
+
+/**
+ * Compatibilidade temporária para branches que ainda referenciam o nome antigo.
+ * Use {@link UsuarioUseCasePort}.
+ */
+@Deprecated
+public interface AuthUsuarioUseCasePort extends UsuarioUseCasePort {
+}
